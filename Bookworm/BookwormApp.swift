2fe2fbfe5313c69eb560d637ed1c51948467284e
Joanna Rodriguez <joannarodriguez134@gmail.com>
2024-07-31 22:26:26 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Joanna Rodriguez on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
